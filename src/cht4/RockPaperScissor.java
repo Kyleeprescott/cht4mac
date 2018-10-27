99bed247aa331e6004054d114743adf911db4753
dustun <dustun@192.168.1.106>
2018-10-26 23:27:45 -0600

    work

---
+++ src/cht4/RockPaperScissor.java
@@ -0,0 +1,26 @@
+package cht4;
+import java.util.Scanner;
+public class RockPaperScissor {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a 0,1,2");
+int num1 = input.nextInt();
+ int num2 = (Math.random());
+
+if (num1 == 0) {
+	System.out.println(" you chose rock");
+	num2 == 0
+	
+}
+else if (num1 == 1) {
+	System.out.println("you chose paper");
+}
+else if (num1 ==2) {
+	System.out.println("you chose scissors ");
+}
+}
+
+}
